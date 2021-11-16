26f7857d6f7afc685c9c585ea25ea5d9eff938b0
= <tmgast@gmail.com>
2021-11-17 06:27:59 +0900

    added some basic route starters and tests definitions

---
+++ app.test.js
@@ -0,0 +1,36 @@
+const request = require('supertest');
+const app = require('./app');
+
+describe('Explore API', () => {
+  it('should run', () => {});
+
+})
+
+describe('users endpoints',() => {
+  it('GET /users --> array users', () => {
+    return request(app)
+      .get('/users')
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then(response => {
+        expect(response.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              name: expect.any(String),
+              id: expect.any(String)
+            })
+        ]))
+      })
+  });
+
+  it('GET /users/id --> specific User Object', () => {});
+
+  it('GET /users/id --> 404 when not found', () => {});
+
+  it('POST /users --> created User', () => {});
+
+  it('GET /users --> ', () => {});
+
+  it('PUT /users/id --> update User', () => {});
+
+});
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var createError = require('http-errors');
+
+const users = [{
+  id: "1",
+  name: "bob",
+  id: "2",
+  name: "jim"
+}];
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json(users);
+});
+
+/* GET users listing. */
+router.get('/:id', function(req, res, next) {
+  const user = users.find(user => user.id === id);
+  if(!user){
+    next(createError(404, "Not Found"));
+  }
+
+  res.json(user);
 });
 
 module.exports = router;
